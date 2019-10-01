937428d29e04b4a262c8c17013345c1d2639ede5
Lisa <lisa.michael@hotmail.com>
2019-09-30 22:53:26 -0400

    this is my javascript for this homework. new

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+ // creating my pseudo code aka love notes to myself
+
+    // create variables i'm going to use in the game. 
+
+    let wins = 0;
+    let guessLetter = 0;
+    // my variables should be stored in an array 
+
+    //press any key to get started
+
+    // computer chosen unguess word will display in dashes
