8f47e3134c3b0d1eb78b90b41a6035efa48bd22b
Lisa <lisa.michael@hotmail.com>
2019-10-02 20:49:51 -0400

    added code when pressing letter to guess at word. Starting for loop to compare ---

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,77 @@
- // creating my pseudo code aka love notes to myself
+// creating my pseudo code aka love notes to myself
 
-    // create variables i'm going to use in the game. 
-    
-    let wins = 0;
-    let guessLetter = 0;
+// create variables i'm going to use in the game. 
+
+let wins = 0;
+let guessLetter = 0;
+
 // let incorrectGuess = 0;
-   
-    // cartoon variables should be stored in an array 
-    let cartoon = ["flintstones", "jetsons" ,"frozen", "garfield", "bambi", "underdog"];
+let incorrectGuess = 0;
 
-    //press any key to get started
+//boolean to check if pressed letter is in work
+var isInWord = false;
 
-    document.onkeyup = function() {
-       //using alert window as test
-      alert("key was pressed. Game good to go");
-      console.log("this is test text");
+// letters user can choose from
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-      //select a random cartoon 
+// cartoon variables should be stored in an array 
+let cartoon = ["flintstones", "jetsons", "frozen", "garfield", "bambi", "underdog", "Popeye"];
 
-      let randomCartoon = cartoon[Math.floor(Math.random() * cartoon.length)];
-      console.log(randomCartoon);
-      alert(randomCartoon);
+//press any key to get started
 
-      // rnadom cartoon/ unguessed word will display in dashes
+document.onkeyup = function () {
+   //using alert window as test
+   alert("key was pressed. Game good to go");
+   console.log("this is test text");
 
-      let unknownAnswer = []; 
-      for (let i=0; i < randomCartoon.length; i++) {
-       unknownAnswer[i] = "_ ";
-         // addDashToArray = "_ " ;
+   //select a random cartoon 
 
-         console.log(unknownAnswer);
-         // console.log(unknownAnswer.join(''));
-      } 
-      // elements.join('');
-       console.log(unknownAnswer);
-       alert(unknownAnswer);
+   let randomCartoon = cartoon[Math.floor(Math.random() * cartoon.length)];
+   console.log(randomCartoon);
+   alert(randomCartoon);
 
-    //array of incorrect guessed letters
+   // rnadom cartoon/ unguessed word will display in dashes
 
-   
+   let unknownAnswer = [];
+   for (let i = 0; i < randomCartoon.length; i++) {
+      unknownAnswer[i] = "_ ";
+
+
+      // console.log(unknownAnswer.join(' '));
+
+   }
+   // elements.join(''); to remove the quotation marks
+   console.log(unknownAnswer.join(' '));
+   alert(unknownAnswer.join(' '));
+
+   //array of incorrect guessed letters
+   // array code here 
+
+
+   //this snippet was taken from the car assignment 
+
+   // when key is pressed, on key up, display the string  
+   // of whatever is pressed and turn to string and make it lowercase
+
+   document.onkeyup = function (event) {
+      let letter = String.fromCharCode(event.keyCode).toLowerCase();
+      alert(letter);
    }
 
-   
- 
-    
+
+   //checking to see if alphabet selected is in randomly chosen Cartoon
+   // https://github.com/the-realest-stu/Hangman-Game/blob/master/assets/javascript/game.js
+
+
+
+   for (let i = 0; i < randomCartoon.length; i++) {
+      if (unknownAnswer.charAt(i) === letter) {
+         isInWord = true;
+
+         alert(unknownAnswer);
+      }
+   }
+}
+
+
+
