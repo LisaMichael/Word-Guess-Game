6d087f6be5e29a3f92aff8f4ffa19150df1971e1
Lisa <lisa.michael@hotmail.com>
2019-10-02 22:48:42 -0400

    modified javascript to convert lowercase to uppercase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,10 +12,11 @@ let incorrectGuess = 0;
 var isInWord = false;
 
 // letters user can choose from
-let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//remming out because it seems i don't need it. leaving in comments, just in case
+// let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // cartoon variables should be stored in an array 
-let cartoon = ["flintstones", "jetsons", "frozen", "garfield", "bambi", "underdog", "Popeye"];
+let cartoon = ["FLINTSTONES", "JETSONS", "FROZEN", "GARFIELD", "BAMBI", "UNDERDOG", "POPEYE"];
 
 //press any key to get started
 
@@ -30,17 +31,15 @@ document.onkeyup = function () {
    console.log(randomCartoon);
    alert(randomCartoon);
 
-   // rnadom cartoon/ unguessed word will display in dashes
+   // random cartoon/ unguessed word will display in dashes
 
    let unknownAnswer = [];
    for (let i = 0; i < randomCartoon.length; i++) {
       unknownAnswer[i] = "_";
 
-
-      // console.log(unknownAnswer.join(' '));
-
    }
    // elements.join(''); to remove the quotation marks
+   //printing to console and alert for testing
    console.log(unknownAnswer.join(' '));
    alert(unknownAnswer.join(' '));
 
@@ -48,43 +47,46 @@ document.onkeyup = function () {
    // array code here 
 
 
-   //this snippet was taken from the car assignment 
+   //this snippet was taken from the car assignment -wk3
 
    // when key is pressed, on key up, display the string  
    // of whatever is pressed and turn to string and make it lowercase
 
    document.onkeyup = function (event) {
-      guessLetter = String.fromCharCode(event.keyCode).toLowerCase();
+      
+      //  randomCartoon = String.fromCharCode(event.keyCode).toLowerCase();
+       guessLetter = String.fromCharCode(event.keyCode).toUpperCase();
       alert(guessLetter);
       console.log(guessLetter);
 
 
-        //checking to see if alphabet selected(pressed) is contained in randomly chosen Cartoon
-   // https://github.com/the-realest-stu/Hangman-Game/blob/master/assets/javascript/game.js
+      //checking to see if alphabet selected(pressed) is contained in randomly chosen Cartoon
+      // https://github.com/the-realest-stu/Hangman-Game/blob/master/assets/javascript/game.js
 
       for (let j = 0; j < randomCartoon.length; j++) {
          if (randomCartoon[j] === guessLetter) {
             isInWord = true;
-             unknownAnswer[j] = guessLetter ;
-   console.log(isInWord);
-            alert(unknownAnswer);
+            unknownAnswer[j] = guessLetter;
+            console.log(isInWord);
+            alert(unknownAnswer.join(' '));
          }
-         else{console.log("im ignoring the if statement");}
+         else { console.log("im ignoring the if statement"); }
+      }
+
+      //checking to see if isInWord is true or false. if false ++
+      if (randomCartoon != guessLetter) {
+         isInWord = false;
+         incorrectGuess++
+         console.log("number of incorrect guesses: " + incorrectGuess);
       }
       
-      //checking to see if isInWord is true or false. if false ++
-      if(randomCartoon !=guessLetter) {
-         isInWord=false;
-      incorrectGuess++
-      console.log("number of incorrect guesses: " + incorrectGuess);
-   }
    }
 
 
- 
 
 
-   
+
+
 }
 
 
