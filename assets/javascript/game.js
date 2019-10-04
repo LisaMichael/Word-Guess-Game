0e261a8bc8f2da9344483f75299aa600faef0980
Lisa <lisa.michael@hotmail.com>
2019-10-03 22:28:32 -0400

    I finally got wins to work properly but eed to check if guess already asked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,9 @@
 
 let wins = 0;
 let guessLetter = 0;
-
-// let incorrectGuess = 0;
 let incorrectGuess = 0;
 
-//boolean to check if pressed letter is in work
+//boolean to check if pressed letter is in word
 var isInWord = false;
 
 // letters user can choose from
@@ -18,7 +16,10 @@ var isInWord = false;
 // cartoon variables should be stored in an array 
 let cartoon = ["FLINTSTONES", "JETSONS", "FROZEN", "GARFIELD", "BAMBI", "UNDERDOG", "POPEYE"];
 
-//press any key to get started
+// lettersInWord = number of characters in the word
+let lettersInWord = 0;
+
+//press any key to get gamestarted
 
 document.onkeyup = function () {
    //using alert window as test
@@ -32,17 +33,21 @@ document.onkeyup = function () {
    alert(randomCartoon);
 
    // random cartoon/ unguessed word will display in dashes
-
    let unknownAnswer = [];
    for (let i = 0; i < randomCartoon.length; i++) {
       unknownAnswer[i] = "_";
-
    }
+
    // elements.join(''); to remove the quotation marks
+
    //printing to console and alert for testing
    console.log(unknownAnswer.join(' '));
    alert(unknownAnswer.join(' '));
 
+
+   // ************************************
+    // ************************************
+     // ************************************
    //array of incorrect guessed letters
    // array code here 
 
@@ -52,12 +57,16 @@ document.onkeyup = function () {
    // when key is pressed, on key up, display the string  
    // of whatever is pressed and turn to string and make it lowercase
 
+
    document.onkeyup = function (event) {
-      
+
       //  randomCartoon = String.fromCharCode(event.keyCode).toLowerCase();
-       guessLetter = String.fromCharCode(event.keyCode).toUpperCase();
+      guessLetter = String.fromCharCode(event.keyCode).toUpperCase();
       alert(guessLetter);
       console.log(guessLetter);
+
+      //TRIED A WHILE STATEMENT BUT NOT WORKING
+      // while (incorrectGuess < 15){
 
 
       //checking to see if alphabet selected(pressed) is contained in randomly chosen Cartoon
@@ -65,29 +74,60 @@ document.onkeyup = function () {
 
       for (let j = 0; j < randomCartoon.length; j++) {
          if (randomCartoon[j] === guessLetter) {
-            isInWord = true;
+           
             unknownAnswer[j] = guessLetter;
+            isInWord = true;
+            lettersInWord++
             console.log(isInWord);
-            alert(unknownAnswer.join(' '));
+            console.log(unknownAnswer.join(' '));
          }
-         else { console.log("im ignoring the if statement"); }
+         
+         }
+        
+       //  ++++++++++++++
+
+      if(!isInWord) {
+         incorrectGuess++;
+         alert(incorrectGuess + " of wrong guesses");
+         console.log(incorrectGuess + " of wrong guesses");
+      }
+// if the # of correct characters in the my guesses equals the length of the word
+// means that i have guessed the word 
+      if (lettersInWord === (unknownAnswer.length)){
+         wins++;
+         console.log('wins ', wins)
       }
 
+      // ***********************************************
+      // ***********************************************
+      // ***********************************************
+
+      // I NEED TO CREATE AN ARRAY THAT STORES THE LETTERS I HAVE GUESSED
+      // I CAN USE THE POP COMMAND TO APPEND TO THE ARRAY
+      // IF THE SIZE OF THE ARRAY IS GREATER THAN 14, GAME OVER 
+
+
+      // ***********************************************
+      // ***********************************************
+      // ***********************************************
+      // I NEED TO CREATE CODE TO DETERMINE IF LETTER WAS ALREADY SELECTED
+
+
+// ***********************************************
+// ***********************************************
+// ***********************************************
+// I NEED TO CREATE CODE TO DETERMINE 
+
       //checking to see if isInWord is true or false. if false ++
-      if (randomCartoon != guessLetter) {
-         isInWord = false;
-         incorrectGuess++
-         console.log("number of incorrect guesses: " + incorrectGuess);
-      }
-      
+      // THIS NEEDS WORK
+      // if (randomCartoon != guessLetter) {
+      //    isInWord = false;
+      //    incorrectGuess++
+      //    console.log("number of incorrect guesses: " + incorrectGuess);
+      // }
+
    }
 
 
-
-
-
-
 }
-
-
 
