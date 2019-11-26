699ed46ab09afce610c4861d0ac953ec9153db71
Lisa <lisa.michael@hotmail.com>
2019-11-25 22:16:07 -0500

    modified reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,8 @@
 let wins = 0; //win-counter
 let guessLetter = 0;
 let wrongGuess = 15;
+let randomCartoon = "";
+
 
 
 //boolean to check if pressed letter is in word
@@ -16,8 +18,6 @@ let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 // cartoon variables should be stored in an array 
 let cartoon = ["FLINTSTONES", "JETSONS", "FROZEN", "GARFIELD", "BAMBI", "UNDERDOG", "POPEYE"];
 
-// testing with 1 array element 
-// let cartoon = ["BAMBI"];
 
 // lettersInWord = number of characters in the word
 let lettersInWord = 0;
@@ -31,20 +31,33 @@ function reset() {
    unknownAnswer = [];
    randomCartoon = cartoon[Math.floor(Math.random() * cartoon.length)];
    //boolean to check if pressed letter is in word
-   isInWord = false;
-   return;
+   unknownAnswer = [];
+   for (let i = 0; i < randomCartoon.length; i++) {
+
+      // elements.join(''); to remove the quotation marks
+      unknownAnswer[i] = "_";
+
+
+      // +++++++++++++++++++
+      //++++++++++++++++++++ Display word to be guessed (dashes) in HTML
+      //++++++++++++++++++++
+
+      html1 = "<p> " + unknownAnswer.join(' ') + "</p>";
+      // console.log(html1);
+      document.querySelector('#word-array').innerHTML = html1;
+      isInWord = false;
+      // return;
+   }
 }
+
 
 //press any key to get gamestarted
 
 document.onkeyup = function () {
-   //using alert and console.log as test
-   // alert("key was pressed. Game good to go");
-   // console.log("this is test text");
 
    //select a random cartoon 
 
-   let randomCartoon = cartoon[Math.floor(Math.random() * cartoon.length)];
+   randomCartoon = cartoon[Math.floor(Math.random() * cartoon.length)];
    console.log(randomCartoon);
    alert(randomCartoon);
 
@@ -55,20 +68,22 @@ document.onkeyup = function () {
 
       // elements.join(''); to remove the quotation marks
       unknownAnswer[i] = "_";
+
+
+      // +++++++++++++++++++
+      //++++++++++++++++++++ Display word to be guessed (dashes) in HTML
+      //++++++++++++++++++++
+
+      let html1 = "<p> " + unknownAnswer.join(' ') + "</p>";
+      // console.log(html1);
+      document.querySelector('#word-array').innerHTML = html1;
+
+      //printing to console and alert for testing
+      console.log(unknownAnswer.join(' '));
+      // alert(unknownAnswer.join(' '));
+      // console.log(unknownAnswer);
+
    }
-
-   // +++++++++++++++++++
-   //++++++++++++++++++++ Display word to be guessed (dashes) in HTML
-   //++++++++++++++++++++
-
-   let html1 = "<p> " + unknownAnswer.join(' ') + "</p>";
-   console.log(html1);
-   document.querySelector('#word-array').innerHTML = html1;
-
-   //printing to console and alert for testing
-   console.log(unknownAnswer.join(' '));
-   alert(unknownAnswer.join(' '));
-   console.log(unknownAnswer);
 
 
 
@@ -79,10 +94,9 @@ document.onkeyup = function () {
    // when key is pressed, on key up, display the string  
    // of whatever is pressed and turn to string and make it uppercase
 
-
    document.onkeyup = function (event) {
 
-
+      // this is the letter i'm guessing 
       //  randomCartoon = String.fromCharCode(event.keyCode).toLowerCase();
       guessLetter = String.fromCharCode(event.keyCode).toUpperCase();
       alert(guessLetter);
@@ -98,7 +112,7 @@ document.onkeyup = function () {
 
             unknownAnswer[j] = guessLetter;
             isInWord = true;
-            console.log(isInWord);
+            console.log(isInWord + " i am hitting this code");
             console.log(unknownAnswer.join(' '));
             lettersInWord++;
 
@@ -112,9 +126,9 @@ document.onkeyup = function () {
          }
 
       }
-console.log("line 115 " + isInWord)
+      console.log("line 115 " + isInWord + wrongGuess);
 
-       // ************************************
+      // ************************************
       // ************************************
       // ************************************
       //array of incorrect guessed letters
@@ -127,9 +141,9 @@ console.log("line 115 " + isInWord)
       // +++++++++++++++ if letter is not in guess word, add to incorrect guesses counter
       //++++++++++++++++
 
-      //THIS IS NOT BEING RECOGNIZED
+      // if the letter is incorrect, decrement the number of guesses left
       if (!isInWord) {
-         console.log("line 132")
+         console.log("this is wrong guess now " + wrongGuess);
          wrongGuess--;
          alert(wrongGuess + " of wrong guesses");
          console.log(wrongGuess + " of wrong guesses");
@@ -145,8 +159,8 @@ console.log("line 115 " + isInWord)
          // now change it back to false
          isInWord = false;
       }
-      
 
+      console.log("line 140 " + isInWord);
       // if the # of correct characters in the my guesses equals the length of the word
       // means that i have guessed the word 
       if (lettersInWord === (unknownAnswer.length)) {
@@ -168,9 +182,10 @@ console.log("line 115 " + isInWord)
 
          reset();
       }
-      if(wrongGuess=0){
+
+      if (wrongGuess < 1) {
          alert("YOU LOSE !!!");
-         reset();
+
       }
 
 
@@ -178,9 +193,9 @@ console.log("line 115 " + isInWord)
 
    }
 
-  
 
-  
+
+
 
    // ***********************************************
    // ***********************************************
@@ -197,7 +212,7 @@ console.log("line 115 " + isInWord)
    // I NEED TO CREATE CODE TO DETERMINE IF LETTER WAS ALREADY SELECTED
 
 
-   
+
 
 }
 
